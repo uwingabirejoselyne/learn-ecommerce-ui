e46aab6f9c0f28ce3880b2f81155701a24e55729
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-18 22:13:24 +0300

    creation of about page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
 
 function App() {
-  return<>
-  <BrowserRouter>
-  <Routes></Routes>
-  </BrowserRouter>
-  </> 
+  return (<>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>)
 }
 
 export default App;
