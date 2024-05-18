1f0d3ffbfa1372fb0dea0be9a85d55b9c0165790
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-18 22:18:24 +0300

    creation of contact page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (<>
@@ -10,6 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path='about' element={<About/>}/>
+          <Route path='contact' element={<Contact/>}/>
+
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Creation of contact page</h2>
+    </div>
+  )
+}
+
+export default Contact
