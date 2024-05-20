f3f3306b00104fa73c84ba2090581629f4e00ea2
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-20 16:41:22 +0200

    styling top header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <div className='header-top-strip py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6 '>
+              <p className='text-white mb-0'>Free Shipping Over$100 $ Free returns</p>
+            </div>
+            <div className='col-6'>
+              <p className='text-end text-white mb-0'>
+                Hotline:<a className='text-white' href='+250785991851'> +25 0785991851</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
