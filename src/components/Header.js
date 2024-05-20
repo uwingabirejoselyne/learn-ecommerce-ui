ab83ad9899484725dd70a2466dc5ac628a7fce9f
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-20 22:14:01 +0200

    upper header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { NavLink, Link } from 'react-router-dom'
+import { BsSearch } from 'react-icons/bs'
 const Header = () => {
   return (
     <>
-      <div className='header-top-strip py-3'>
+      <header className='header-top-strip py-3'>
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-6 '>
@@ -16,7 +17,25 @@ const Header = () => {
             </div>
           </div>
         </div>
-      </div>
+      </header>
+      <header className='header-upper py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-2'>
+              <h2><Link className='text-white'>Dev Corner </Link></h2>
+            </div>
+            <div className='col-5'>
+              <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Search product here" aria-label="Search product here" aria-describedby="basic-addon2" />
+                <span className="input-group-text" id="basic-addon2"><BsSearch/></span>
+              </div>
+            </div>
+            <div className='col-5'>
+              <h2>Dev Corner</h2>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   )
 }
