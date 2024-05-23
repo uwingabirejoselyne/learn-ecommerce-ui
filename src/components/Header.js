c39321d85feac75b03f76b39ae208bf4ecc1be02
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-23 17:12:12 +0200

    header-upper-links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {  Link } from 'react-router-dom'
+import {  Link, NavLink } from 'react-router-dom'
 import { BsSearch } from 'react-icons/bs'
 const Header = () => {
   return (
@@ -63,6 +63,25 @@ const Header = () => {
               </div>
             </div>
           </div>
+        </div>
+      </header>
+      <header className='header-bottom py-3'>
+        <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-12'>
+            <div className='menu-bottom d-flex align-items-center'>
+              <div></div>
+              <div className='menu-links'>
+                <div className='d-flex align-items-center gap-15'>
+                  <NavLink to='/'>Home</NavLink>
+                  <NavLink to='/'>Our Store</NavLink>
+                  <NavLink to='/'>Blogs</NavLink>
+                  <NavLink to='/'>Contact</NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         </div>
       </header>
     </>
