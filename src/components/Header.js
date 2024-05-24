118021c3acf832d24d7b2133f1e6405d12b66767
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-24 17:30:50 +0200

    dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {  Link, NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { BsSearch } from 'react-icons/bs'
 const Header = () => {
   return (
@@ -27,36 +27,36 @@ const Header = () => {
             <div className='col-5'>
               <div className="input-group mb-3">
                 <input type="text" className="form-control py-2" placeholder="Search product here" aria-label="Search product here" aria-describedby="basic-addon2" />
-                <span className="input-group-text p-3" id="basic-addon2"><BsSearch/></span>
+                <span className="input-group-text p-3" id="basic-addon2"><BsSearch /></span>
               </div>
             </div>
             <div className='col-5'>
               <div className='header-upper-links d-flex align-items-center justify-content-between'>
                 <div>
                   <Link className='d-flex align-items-center gap-10 text-white'>
-                  <img src='images/compare.svg' alt='compare'></img>
-                  <p className='mb-0'>Compare <br/>Products</p>
+                    <img src='images/compare.svg' alt='compare'></img>
+                    <p className='mb-0'>Compare <br />Products</p>
                   </Link>
                 </div>
                 <div>
-                <Link className='d-flex align-items-center gap-10 text-white'>
-                  <img src='images/wishlist.svg' alt='wishlist'></img>
-                  <p className='mb-0'>Favourite<br/>Wishlist</p>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/wishlist.svg' alt='wishlist'></img>
+                    <p className='mb-0'>Favourite<br />Wishlist</p>
                   </Link>
                 </div>
                 <div>
-                <Link className='d-flex align-items-center gap-10 text-white'>
-                  <img src='images/user.svg' alt='user'></img>
-                  <p className='mb-0'>Login <br/>My Account</p>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/user.svg' alt='user'></img>
+                    <p className='mb-0'>Login <br />My Account</p>
                   </Link>
                 </div>
                 <div>
-                <Link className='d-flex align-items-center gap-10 text-white'> 
-                  <img src='images/cart.svg' alt='hhh'></img>
-                  <div className='d-flex flex-column gap-10'>
-                    <span className='badge bg-white text-dark'>0</span>
-                    <p className='mb-0'>$ 500</p>
-                  </div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/cart.svg' alt='hhh'></img>
+                    <div className='d-flex flex-column gap-10'>
+                      <span className='badge bg-white text-dark'>0</span>
+                      <p className='mb-0'>$ 500</p>
+                    </div>
                   </Link>
                 </div>
 
@@ -67,21 +67,33 @@ const Header = () => {
       </header>
       <header className='header-bottom py-3'>
         <div className='container-xxl'>
-        <div className='row'>
-          <div className='col-12'>
-            <div className='menu-bottom d-flex align-items-center'>
-              <div></div>
-              <div className='menu-links'>
-                <div className='d-flex align-items-center gap-15'>
-                  <NavLink to='/'>Home</NavLink>
-                  <NavLink to='/'>Our Store</NavLink>
-                  <NavLink to='/'>Blogs</NavLink>
-                  <NavLink to='/'>Contact</NavLink>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='menu-bottom d-flex align-items-center gap-30'>
+                <div>
+                  <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center justify-content-center " type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src='images/menu.svg' alt='menu'/>
+                    <span className='me-5 d-inline-block'>Shop Categories</span>
+                    </button>
+                    <ul className="dropdown-menu">
+                      <li><Link className="dropdown-item text-white" to='' >Action</Link></li>
+                      <li><Link className="dropdown-item text-white" to=''>Another action</Link></li>
+                      <li><Link className="dropdown-item text-white" to=''>Something else here</Link></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='menu-links'>
+                  <div className='d-flex align-items-center gap-15'>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/'>Our Store</NavLink>
+                    <NavLink to='/'>Blogs</NavLink>
+                    <NavLink to='/'>Contact</NavLink>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
         </div>
       </header>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>Hello from home</h1>
     </div>
   )
 }
