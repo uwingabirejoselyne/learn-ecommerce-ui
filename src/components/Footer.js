78167c32b619991fa778e5344e6aa533a40daf04
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-24 23:23:56 +0200

    upper footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,18 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-    <footer className='py-3'></footer>
+    <footer className='py-3'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-5'>
+            <div className='footer-top-data d-flex gap-30 align-items-center'>
+              <img src='images/newsletter.png' />
+              <h2>Sign Up for NewsLetter</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
     <footer className='py-3'></footer>
     <footer className='py-3'>
       <div className='container-xxl'>
