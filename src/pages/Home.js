d379abacbc4ca76eec01ff401f3dde3322eb542f
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-27 21:02:51 +0200

    cat banner image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,6 +19,17 @@ const Home = () => {
             </div>
           </div>
           <div className='col-6'>
+            <div className='d-flex flex-wrap'>
+            <div className='small-banner position-relative p-3'>
+            <img src='images/catbanner-01.jpg' className='img-fluid rounded-3' alt='main-banner'/>
+            <div className='small-banner-content position-absolute'>
+              <h4>SUPERCHARGED FOR PROS.</h4>
+              <h5>ipad s13+ pro</h5>
+              <p>from $999.99 or $41.62/no</p>
+              <Link className='button'>Buy now</Link>
+            </div>
+            </div>
+            </div>
           </div>
         </div>
       </div>
