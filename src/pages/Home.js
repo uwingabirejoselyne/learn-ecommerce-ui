2bc63a51f70a8916892b8dad46b7457ea1e790c2
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-27 07:55:25 +0200

    main banner image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <div className='row'>
           <div className='col-6'>
             <div className='main-banner p-3'>
-            <img src='images/main-banner.jpg'/>
+            <img src='images/main-banner.jpg' className='img-fluid rounded-3' alt='main-banner'/>
             </div>
           </div>
           <div className='col-6'>
