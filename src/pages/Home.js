1ca0a224ca884a70761548cebaa7bd519fa7fc58
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-27 07:53:00 +0200

    main banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <p>home</p>
-    </div>
+    <>
+    <section>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-6'>
+            <div className='main-banner p-3'>
+            <img src='images/main-banner.jpg'/>
+            </div>
+          </div>
+          <div className='col-6'>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
