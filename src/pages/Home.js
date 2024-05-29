f3518d29c6a9b2e4e32885b234ed18fa4391f5a5
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-05-29 09:39:23 +0200

    best sale and super arival section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,13 +19,38 @@ const Home = () => {
             </div>
           </div>
           <div className='col-6'>
-            <div className='d-flex flex-wrap'>
+            <div className='d-flex flex-wrap gap-10 justify-content-between align-items-center'>
             <div className='small-banner position-relative p-3'>
             <img src='images/catbanner-01.jpg' className='img-fluid rounded-3' alt='main-banner'/>
             <div className='small-banner-content position-absolute'>
-              <h4>SUPERCHARGED FOR PROS.</h4>
+              <h4>BEST SALE</h4>
               <h5>ipad s13+ pro</h5>
-              <p>from $999.99 or $41.62/no</p>
+              <p>from $999.99 <br/> or $41.62/no</p>
+            </div>
+            </div>
+
+            <div className='small-banner position-relative p-3'>
+            <img src='images/catbanner-02.jpg' className='img-fluid rounded-3' alt='main-banner'/>
+            <div className='small-banner-content position-absolute'>
+              <h4>SUPER ARRIVAL</h4>
+              <h5>ipad s13+ pro</h5>
+              <p>from $999.99<br/> or $41.62/no</p>
+            </div>
+            </div>
+            <div className='small-banner position-relative p-3'>
+            <img src='images/catbanner-03.jpg' className='img-fluid rounded-3' alt='main-banner'/>
+            <div className='small-banner-content position-absolute'>
+              <h4>SUPER ARRIVAL</h4>
+              <h5>ipad s13+ pro</h5>
+              <p>from $999.99<br/> or $41.62/no</p>
+            </div>
+            </div>
+            <div className='small-banner position-relative p-3'>
+            <img src='images/catbanner-04.jpg' className='img-fluid rounded-3' alt='main-banner'/>
+            <div className='small-banner-content position-absolute'>
+              <h4>SUPER ARRIVAL</h4>
+              <h5>ipad s13+ pro</h5>
+              <p>from $999.99<br/> or $41.62/no</p>
             </div>
             </div>
             </div>
