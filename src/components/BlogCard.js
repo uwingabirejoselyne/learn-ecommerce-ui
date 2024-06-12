9d0d5c1f79069b8b53fb53e70ae3af379ebbf5d6
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-06-12 17:21:48 +0200

    blog content

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const BlogCard = () => {
   return (
@@ -6,6 +7,16 @@ const BlogCard = () => {
         <div className='blog-card'>
             <div className='card-image'>
                 <img src='images/blog-1.jpg' className='img-fluid' alt='blog'></img>
+            </div>
+            <div className='blog-content'>
+                <p className='date'> 1, Dec 2024</p>
+                <h5 className='title'>
+                    A beautiful sunday  morning renaisance
+                </h5>
+                <p className='desc'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate deserunt omnis voluptates? Reprehenderit assumenda asperiores dolorum, deleniti exercitationem excepturi eveniet!
+                </p>
+                <Link to='/' className='button'>Read More</Link>
             </div>
         </div>
     </div>
