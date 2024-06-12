23f67f2a501a499a7f7ee4958d369ba9d4d3ca4f
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-06-12 08:35:45 +0200

    card-wrapper section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -179,27 +179,30 @@ const Home = () => {
         <div className='row'>
           <div className='col-12'>
             <div className='marquee-inner-wrapper card-wrapper'>
-              <Marquee>
-                <div>
-                  <img src='' alt=''/>
+              <Marquee className='d-flex'>
+                <div className='mx-4 w-25'>
+                  <img src='images/brand-01.png' alt='brand'/>
                 </div>
-                <div>
-                  <img src='' alt=''/>
+                <div className='mx-4 w-25'>
+                  <img src='images/brand-02.png' alt='brand'/>
                 </div>
-                <div>
-                  <img src='' alt=''/>
+                <div className='mx-4 w-25'>
+                  <img src='images/brand-03.png' alt='brand'/>
                 </div>
-                <div>
-                  <img src='' alt=''/>
+                <div className='mx-4 w-25'>
+                  <img src='images/brand-04.png' alt='brand'/>
                 </div>
-                <div>
-                  <img src='' alt=''/>
+                <div className='mx-4 w-25'>
+                  <img src='images/brand-05.png' alt='brand'/>
                 </div>
-                <div>
-                  <img src='' alt=''/>
+                <div className='mx-4 w-25'>
+                  <img src='images/brand-06.png' alt='brand'/>
                 </div>
-                <div>
-                  <img src='' alt=''/>
+                <div className='mx-4 w-25'>
+                  <img src='images/brand-07.png' alt='brand'/>
+                </div>
+                <div className='mx-4 w-25'>
+                  <img src='images/brand-08.png' alt='brand'/>
                 </div>
               </Marquee>
             </div>
