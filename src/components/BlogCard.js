743f750e5fac82aed84cc2f96aab4a0f151434ff
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-06-12 10:24:34 +0200

    blog card component styling

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -3,7 +3,11 @@ import React from 'react'
 const BlogCard = () => {
   return (
     <div className='col-3'>
-        <div className='blog-card'></div>
+        <div className='blog-card'>
+            <div className='card-image'>
+                <img src='images/blog-1.jpg' alt='blog'></img>
+            </div>
+        </div>
     </div>
   )
 }
