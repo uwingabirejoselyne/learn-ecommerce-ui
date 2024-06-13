44633d36ad240cbf777da80108321edb3886ee54
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-06-13 11:59:20 +0200

    action bar part

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ReactStars from 'react-rating-stars-component'
+import { Link } from 'react-router-dom'
 
 const ProductCard = () => {
   return (
     <div className='col-3'>
-      <div className='product-card'>
+      <div className='product-card position-relative'>
         <div className='product-image'>
             <img src='images/watch.jpg'/>
         </div>
@@ -12,7 +14,21 @@ const ProductCard = () => {
             <h5 className='product-title'>
                 Kids headphones bulk 10 pack multi colored for students
             </h5>
+            <ReactStars count={5} size={24} value='3' activeColor='#ffd700' edit={false}/>
             <p className="price">$100.00</p>
+        </div>
+        <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+                <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+                </Link>
+                <Link>
+                <img src="images/view.svg" alt="view" />
+                </Link>
+                <Link>
+                <img src="images/add-cart.svg" alt="addCart" />
+                </Link>
+            </div>
         </div>
       </div>
     </div>
