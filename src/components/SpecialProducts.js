67f609854c982529b43ca0ec802e3694dc4b5a23
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-06-18 15:52:31 +0200

    progress bar

--- src/components/SpecialProducts.js
+++ src/components/SpecialProducts.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactStars from 'react-rating-stars-component'
+import { Link } from 'react-router-dom'
 
 const SpecialProducts = () => {
   return (
@@ -15,12 +16,12 @@ const SpecialProducts = () => {
               Samsung Galaxy Note 10+ <br />
               Mobile Phone:Sim...
             </h6>
-            <ReactStars 
-            count={5} 
-            size={24} 
-            value='4' 
-            activeColor='#ffd700' 
-            edit={false}/>
+            <ReactStars
+              count={5}
+              size={24}
+              value='4'
+              activeColor='#ffd700'
+              edit={false} />
             <p className="price">
               <span className="red-p">
                 $100
@@ -32,11 +33,25 @@ const SpecialProducts = () => {
                 <b>5</b>days
               </p>
               <div className="d-flex gap-10 align-items-center">
-                <span className='badge rounded-circle p-3 bg-warning'>1</span>:
-                <span className='badge rounded-circle p-3 bg-warning'>1</span>:
-                <span className='badge rounded-circle p-3 bg-warning'>1</span>
+                <span className='badge rounded-circle p-3 bg-danger'>1</span>:
+                <span className='badge rounded-circle p-3 bg-danger'>1</span>:
+                <span className='badge rounded-circle p-3 bg-danger'>1</span>
               </div>
             </div>
+            <div className="prod-count my-3">
+                <p>Products: 5</p>
+                <div className="progress">
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "25%" }}
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100">
+                  </div>
+                </div>
+              </div>
+              <Link className='button'>Add to Cart</Link>
           </div>
         </div>
       </div>
