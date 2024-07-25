be897a2c35a4706402ae069aecd776a8b23b8a03
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-07-25 12:19:34 +0200

    meta component

---
+++ src/components/Meta.js
@@ -0,0 +1,11 @@
+import { Helmet } from 'react-helmet'
+const Meta = () => {
+    return (
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Our store</title>
+        </Helmet>
+    )
+}
+
+export default Meta
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import BreadCrumb from '../components/BreadCrumb'
+import { Helmet } from "react-helmet";
+
 
 const OurStore = () => {
   return (
     <>
-      <BreadCrumb  title="our store"/>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Our store</title>
+      </Helmet>
+      <BreadCrumb title="our store" />
     </>
   )
 }
