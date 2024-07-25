ca89e8a9f1e9f4fb589a747a2532a10690ef5adb
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-07-25 09:46:48 +0200

    react-helmet installation

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 const BreadCrumb = (props) => {
     const {title} = props;
   return (
-    <div className='breadcrumb'>
+    <div className='breadcrumb mb-0 py-4'>
         <div className="container-xxl">
             <div className="row">
                 <div className="col-12">
-                    <p className='text-center'>
+                    <p className='text-center mb-0'>
                         <Link to="/" className='text-dark'>Home</Link>
                         / {title}
                     </p>
