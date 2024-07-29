21458353948733a31244d6612f754574d0e1406d
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-07-29 19:13:09 +0200

    added product tags

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -91,6 +91,22 @@ const OurStore = () => {
               </div>
               <div className='filter-card mb-3'>
                 <h3 className="filter-title"> Product Tags</h3>
+                <div>
+                  <div className='product-tags d-flex flex-wrap align-items-center gap-10'>
+                    <spam className='badge bg-light text-secondary roundend-3 py-2 px-3'>
+                  Headphone
+                    </spam>
+                    <spam className='badge bg-light text-secondary roundend-3 py-2 px-3'>
+                  Laptop
+                    </spam>
+                    <spam className='badge bg-light text-secondary roundend-3 py-2 px-3'>
+                  Mobile
+                    </spam>
+                    <spam className='badge bg-light text-secondary roundend-3 py-2 px-3'>
+                  Wire
+                    </spam>
+                  </div>
+                </div>
               </div>
               <div className='filter-card mb-3'>
                 <h3 className="filter-title"> Random Product</h3>
