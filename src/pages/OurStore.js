9308197dd6110794b93c3b6b5a750a6c76ee30f7
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-07-30 10:23:41 +0200

    added random product part

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BreadCrumb from '../components/BreadCrumb'
 import Meta from '../components/Meta'
+import ReactStars from 'react-rating-stars-component'
 
 
 const OurStore = () => {
@@ -110,6 +111,28 @@ const OurStore = () => {
               </div>
               <div className='filter-card mb-3'>
                 <h3 className="filter-title"> Random Product</h3>
+                <div>
+                  <div className="random-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img src="images/watch.jpg" className='img-fluid' alt="watch" />
+                    </div>
+                    <div className='w-75'>
+                      <h5>Kids headphones bulk 10 pack multi colored for students</h5>
+                      <ReactStars count={5} size={24} value='4' activeColor='#ffd700' edit={false}/>
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                  <div className="random-products d-flex">
+                    <div className="w-50">
+                      <img src="images/watch.jpg" className='img-fluid' alt="watch" />
+                    </div>
+                    <div className='w-75'>
+                      <h5>Kids headphones bulk 10 pack multi colored for students</h5>
+                      <ReactStars count={5} size={24} value='4' activeColor='#ffd700' edit={false}/>
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
