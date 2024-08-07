4bab3319dc17a9539576d28af969f3fc1a378838
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-07 15:30:50 +0200

    created Login page

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
+
+const Login = () => {
+  return (
+    <>
+         <Meta title={"Wishlist"} />
+         <BreadCrumb title="Wishlist" />
+    </>
+  )
+}
+
+export default Login
