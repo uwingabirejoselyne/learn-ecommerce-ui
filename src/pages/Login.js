e4049dfddfe9b4db928b3d60b898a6863b3629f0
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-07 17:13:12 +0200

    created forgot password page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,12 +3,33 @@ import Meta from '../components/Meta'
 import BreadCrumb from '../components/BreadCrumb'
 
 const Login = () => {
-  return (
-    <>
-         <Meta title={"Login"} />
-         <BreadCrumb title="Login" />
-    </>
-  )
+    return (
+        <>
+            <Meta title={"Login"} />
+            <BreadCrumb title="Login" />
+            <div className="login-wrapper home-wrapper-2 py-5">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="login-card">
+                                    <h3>Login</h3>
+                                    <form action="">
+                                        <div>
+                                            <input type="email" name='email' placeholder='Email' className='form-control' />
+                                        </div>
+                                        <div>
+                                            <input type="password" name='password' placeholder='Password' className='form-control' />
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Login
