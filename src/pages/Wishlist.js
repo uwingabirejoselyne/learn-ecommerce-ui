1e363e00583854a1944adf47923b6ed5c3fafb4c
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-07 14:28:51 +0200

    created wishlist page

---
+++ src/pages/Wishlist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
+
+const Wishlist = () => {
+    return (
+        <>
+            <Meta title={"Compare Product"} />
+            <BreadCrumb title="Compare Product" />
+        </>
+    )
+}
+
+export default Wishlist
