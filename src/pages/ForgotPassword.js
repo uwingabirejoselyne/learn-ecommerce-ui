83602b95a0109edbea4d9fd53fd9a309b72f4417
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-08 17:49:53 +0200

    finalising forgot password page

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -14,19 +14,16 @@ const ForgotPassword = () => {
             <div className="row">
               <div className="col-12">
                 <div className="auth-card">
-                  <h3 className='text-center mb-3'>Login</h3>
+                <h3 className='text-center mb-3'>Reset Your Password</h3>
+                <p className='text-center my-2 mb-3'>We will send you an email to reset your password</p>
                   <form action="" className='d-flex flex-column gap-15'>
                     <div>
                       <input type="email" name='email' placeholder='Email' className='form-control' />
                     </div>
-                    <div className='mt-1'>
-                      <input type="password" name='password' placeholder='Password' className='form-control' />
-                    </div>
                     <div className=''>
-                      <Link to='forgot-password'>Forget Password</Link>
-                      <div className='mt-3 d-flex justify-content-center gap-15 align-items-center'>
-                        <button className="button border-0">Login</button>
-                        <Link className="button signup">Signup</Link>
+                      <div className='mt-3 d-flex justify-content-center flex-column gap-15 align-items-center'>
+                        <button className="button border-0" type='submit'>Submit</button>
+                        <Link to="/login">Cancel</Link>
                       </div>
                     </div>
                   </form>
