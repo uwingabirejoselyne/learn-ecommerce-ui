28e8a1739e50e55453b3a33430e01e21f5c505a0
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-08 15:42:44 +0200

    finalising login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Meta from '../components/Meta'
 import BreadCrumb from '../components/BreadCrumb'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
@@ -12,14 +13,21 @@ const Login = () => {
                     <div className="row">
                         <div className="row">
                             <div className="col-12">
-                                <div className="login-card">
-                                    <h3>Login</h3>
-                                    <form action="">
+                                <div className="auth-card">
+                                    <h3 className='text-center mb-3'>Login</h3>
+                                    <form action="" className='d-flex flex-column gap-15'>
                                         <div>
                                             <input type="email" name='email' placeholder='Email' className='form-control' />
                                         </div>
-                                        <div>
+                                        <div className='mt-1'>
                                             <input type="password" name='password' placeholder='Password' className='form-control' />
+                                        </div>
+                                        <div className=''>
+                                            <Link to='forgot-password'>Forget Password</Link>
+                                            <div className='mt-3 d-flex justify-content-center gap-15 align-items-center'>
+                                                <button className="button border-0">Login</button>
+                                                <Link className="button signup">Signup</Link>
+                                            </div>
                                         </div>
                                     </form>
                                 </div>
